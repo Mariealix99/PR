cdc18a2e30a7a710e2664894a61fe80f4df35cee
Mariealix99 <mariealixgillyboeuf@gmail.com>
2025-11-24 12:23:02 +0100

    sound mapped to brightness

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763980816|1420489297';
+const CACHE_VERSION = '1763983249|3853342548';
 /** @type {string} */
 const CACHE_PREFIX = 'Sonification-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
